1e917e1ff6675d235afa2ed4fcbb42156570edc5
LimeThaw <laudit@student.ethz.ch>
2017-05-15 23:44:24 +0200

    Added comparison graphs and data file

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import re
+import json
 
 infile = open("out.txt", "r")
 values = infile.read()
@@ -23,6 +24,8 @@ for val in values:
 	avgConnectedness.append(val[6])
 
 length = len(popCount)
+
+cmpData = json.loads(open("data.json").read())
 
 # See https://matplotlib.org/api/pyplot_api.html?highlight=plot#matplotlib.pyplot.plot for
 # plot styles
@@ -64,4 +67,61 @@ plt.plot(
 plt.xlabel("Day")
 plt.legend()
 plt.axis([0, length, 0, 120])
+
+plt.figure()
+
+populationChange = [0] + map(lambda p, pp: (float(p)-float(pp))/float(p)*100000, popCount[1:], popCount[:-1])
+violentCrimes = map(lambda c, p: float(c)/float(p)*100000, murders, popCount)
+firearmCrimes = map(lambda c, p: float(c)/float(p)*100000, gunMurders, popCount)
+
+plt.plot(
+	[0, length-1],
+	[cmpData["populationChange"]]*2,
+	'g-',
+	label="Population change rate in the US"
+)
+plt.plot(
+	range(0, length),
+	populationChange,
+	'g:',
+	label="Population change in our model"
+)
+plt.plot(
+	[0, length-1],
+	[cmpData["violentCrimes"]]*2,
+	'b-',
+	label="Violent crime rate in the US"
+)
+plt.plot(
+	range(0, length),
+	violentCrimes,
+	'b:',
+	label="Violent crime rate in our model"
+)
+plt.plot(
+	[0, length-1],
+	[cmpData["firearmCrimes"]]*2,
+	'r-',
+	label="Firearm crimes in the US"
+)
+plt.plot(
+	range(0, length),
+	firearmCrimes,
+	'r:',
+	label="Firearm crime rate in our model"
+)
+plt.xlabel("Day")
+plt.legend()
+plt.axis([0, length, 0, 10])
+
+fail = 0.0
+for i in range(0, length):
+	fail += (cmpData["violentCrimes"] - violentCrimes[i])**2
+	fail += (cmpData["firearmCrimes"] - firearmCrimes[i])**2
+
+for i in range(1, length): # Start from one because on day 0 change is always 0
+	fail += (cmpData["populationChange"] - populationChange[i])**2
+
+print("Square sum error: " + str(fail))
+
 plt.show()
